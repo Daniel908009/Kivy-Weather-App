3c6730fc81af6efa56209f524b61e471c1da0092
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-22 20:35:06 +0100

    Added background images, added dynamic switching of the images, added dynamic resize of the images.
    Also added loading cities from a file into the scrollview.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.uix.effectwidget import Rectangle
 from kivy.uix.popup import Popup
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -88,10 +89,14 @@ class CityWidget(BoxLayout):
         self.caller.caller.currentCity = self.ids.cityName.text
 
 class MainGrid(BoxLayout):
+    def __init__(self):
+        super().__init__()
+        self.bind(size = self.setBackground, pos = self.setBackground)
     def load_city(self):
         info = get_weather(self.currentCity)
         #print("Here")
         if info != None:
+            self.weather = str(info["weather"][0]["description"])
             self.ids.cityName.text = str(self.currentCity)
             #print(info["weather"][0]["description"])
             self.ids.weather.text = str(info["weather"][0]["description"])
@@ -100,16 +105,53 @@ class MainGrid(BoxLayout):
             #print(info["main"]["humidity"])
             self.ids.humidity.text = str(info["main"]["humidity"]) + "%"
 
+            # getting info for the sunrise and sunset
+            self.sunrise = info["sys"]["sunrise"]
+            self.sunset = info["sys"]["sunset"]
+            self.timeIn = info["dt"]
+            # this is just for testing
+            #print(self.sunrise, self.sunset, self.timeIn)
+            #print(type(self.sunrise), type(self.sunset), type(self.timeIn))
+            #if self.timeIn >= self.sunset or self.timeIn <= self.sunrise:
+            #    print("Night")
     def addCity(self):
         popup = AddCityPopup(self)
         popup.open()
     def setup(self):
-        self.getCurrentCity()
+        self.getCities()
         self.load_city()
+        self.setBackground()
+    def setBackground(self, *args):
+        if self.weather != "":
+            #print(self.weather)
+            self.canvas.before.clear()
+            if self.checkTime():
+                self.canvas.before.add(Rectangle(size = self.size, pos = self.pos, source="night.jpg"))
+                #print("Night")
+            elif "rain" in self.weather or "thunderstorm" in self.weather or "drizzle" in self.weather:
+                self.canvas.before.add(Rectangle(source="rain.jpg", size = self.size, pos = self.pos))
+                #print("Rain")
+            elif "cloud" in self.weather:
+                self.canvas.before.add(Rectangle(source="cloudy.jpg", size = self.size, pos = self.pos))
+                #print("Cloudy")
+            elif "clear" in self.weather:
+                self.canvas.before.add(Rectangle(source="sunny.jpg", size = self.size, pos = self.pos))
+                #print("Clear")
+            elif "snow" in self.weather:
+                self.canvas.before.add(Rectangle(source="snow.jpg", size = self.size, pos = self.pos)) # will be added later
+                #print("Snow")
+            else:
+                self.canvas.before.add(Rectangle(source="bigMystery.jpg", size = self.size, pos = self.pos)) # will be added later
+                #print("Default")
+    def checkTime(self):
+        if self.timeIn >= self.sunset or self.timeIn <= self.sunrise:
+            return True
+        else:
+            return False
     def listOfCitiesPopup(self):
         popup = ListOfCitiesPopup(self)
         popup.open()
-    def getCurrentCity(self):
+    def getCities(self):
         try:
             with open("data.json", "r") as file:
                 data = json.load(file)
@@ -118,11 +160,12 @@ class MainGrid(BoxLayout):
                 #print(key)
                 if key["CurrentCity"] == "Yes":
                     self.currentCity = key["Name"]
-                    break
+                self.cities.append(key["Name"])
         except:
             #print("Error in reading the file")
             with open("data.json", "w") as file:
                 json.dump({"Name": "Paris", "CurrentCity": "No"}, file)
+            file.close()
 
 class WeatherApp(App):
     def build(self):
