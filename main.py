3513f340fd6fc1c1b15e5b348b0db2559381b2f3
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-22 16:41:01 +0100

    Did some basic UI of the app.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 import requests
 
 
-API = ""
+WEATHERAPI = "" # used to get the weather data
+CITYAPI = "" # used to get the coordinates of a city
 
 def get_weather(city):
     lon, lat = 14.4378, 50.0755 # will later be replaced with the actual coordinates from a city entered by the user
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API}")
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHERAPI}")
     if response.status_code == 200:
         data = response.json()
         print(data)
@@ -17,15 +19,16 @@ def get_weather(city):
 
 
 
-class MainGrid(GridLayout):
+class MainGrid(BoxLayout):
     def test_request(self):
-        get_weather("London")
+        get_weather("London") # later the city will be fetched from an input field
 
 
 
 
 class WeatherApp(App):
     def build(self):
+        Window.clearcolor = (1, 1, 1, 1)
         return MainGrid()
     
 if __name__ == "__main__":
