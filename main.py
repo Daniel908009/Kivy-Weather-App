3845234879f97831184772333272f59b967d22ae
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-22 19:03:14 +0100

    Added a saving system in json and more api stuff.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 import requests
-
+import json
 
 WEATHERAPI = "" # used to get the weather data
 CITYAPI = "" # used to get the coordinates of a city
@@ -19,11 +19,14 @@ def get_weather(city):
         response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={WEATHERAPI}")
         if response.status_code == 200:
             data = response.json()
-            print(data)
+            #print(data)
+            return data
         else:
-            print("Error in fetching the data")
+            #print("Error in fetching the data")
+            return None
     else:
-        print("Error in fetching the data")
+        #print("Error in fetching the data")
+        return None
 
 class ConfirmPopup(Popup):
     def __init__(self, caller):
@@ -85,14 +88,41 @@ class CityWidget(BoxLayout):
         self.caller.caller.currentCity = self.ids.cityName.text
 
 class MainGrid(BoxLayout):
-    def test_request(self):
-        get_weather("Hradec nad Moravici") # later the city will be fetched from an input field
+    def load_city(self):
+        info = get_weather(self.currentCity)
+        #print("Here")
+        if info != None:
+            self.ids.cityName.text = str(self.currentCity)
+            #print(info["weather"][0]["description"])
+            self.ids.weather.text = str(info["weather"][0]["description"])
+            #print(info["main"]["temp"])
+            self.ids.temperature.text = str(round(info["main"]["temp"] - 273.15, 2)) + "°C"
+            #print(info["main"]["humidity"])
+            self.ids.humidity.text = str(info["main"]["humidity"]) + "%"
+
     def addCity(self):
         popup = AddCityPopup(self)
         popup.open()
+    def setup(self):
+        self.getCurrentCity()
+        self.load_city()
     def listOfCitiesPopup(self):
         popup = ListOfCitiesPopup(self)
         popup.open()
+    def getCurrentCity(self):
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+            file.close()
+            for key in data:
+                #print(key)
+                if key["CurrentCity"] == "Yes":
+                    self.currentCity = key["Name"]
+                    break
+        except:
+            #print("Error in reading the file")
+            with open("data.json", "w") as file:
+                json.dump({"Name": "Paris", "CurrentCity": "No"}, file)
 
 class WeatherApp(App):
     def build(self):
